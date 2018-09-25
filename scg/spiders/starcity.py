b06ec32c38c6fc7b6adb4877c21cd960501b0ff9
Kevin Tovar <kevintovar@esimez.mx>
2018-09-24 23:18:19 -0500

    Update scraper

--- scg/spiders/starcity.py
+++ scg/spiders/starcity.py
@@ -1,18 +1,16 @@
 import scrapy
+import collections
 
 class SimpleScrapper(scrapy.Spider):
     name = "simple"
 
     def start_requests(self):
         urls = [
-            'http://www.starcitygames.com/catalog/category/Kaladesh',
-            'http://www.starcitygames.com/catalog/category/Aether%20Revolt',
-            'http://www.starcitygames.com/catalog/category/Amonkhet',
-            'http://www.starcitygames.com/catalog/category/Hour%20of%20Devastation',
             'http://www.starcitygames.com/catalog/category/Explorers%20of%20Ixalan',
             'http://www.starcitygames.com/catalog/category/Rivals%20of%20Ixalan',
             'http://www.starcitygames.com/catalog/category/Dominaria',
-            'http://www.starcitygames.com/catalog/category/Core%20Set%202019'
+            'http://www.starcitygames.com/catalog/category/Core%20Set%202019',
+            #'http://www.starcitygames.com/catalog/category/Guilds%20of%20Ravnica'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -26,17 +24,26 @@ class SimpleScrapper(scrapy.Spider):
                     expansion = (
                         item.css('.search_results_2 a::text').extract_first().rstrip()
                     )
-                    name = name.replace('\n', '')
-                    price = item.css('.search_results_9::text').extract_first().replace('$', '')
-                    price = float(price)
-                    yield(dict(
-                        name=name,
-                        expansion=expansion,
-                        price=price,
-                        mxn_regular=price * 16,
-                        mxn_client=price * 15
-                    ))
+                    rarity = item.css('.search_results_6::text').extract_first()
+                    if rarity in ['R', 'M']:
+                        id = item.css('.search_results_1 b a::attr(href)').extract_first().replace('http://www.starcitygames.com/catalog/magic_the_gathering/product/', '')
+                        if '/' in id:
+                            id = id.split('/')[0]
+                        print(id)
+                        name = name.replace('\n', '')
+                        price = item.css('.search_results_9::text').extract_first().replace('$', '')
+                        price = float(price)
+                        data = collections.OrderedDict()
+                        data['id'] = id
+                        data['name'] = name
+                        data['expansion'] = expansion
+                        data['rarity'] = rarity
+                        data['price'] = price
+                        data['mxn_regular'] = price * 17
+                        data['mxn_client'] = price * 16
+                        yield(data)
                 except Exception as e:
+                    print(e)
                     print("Card couldn't be processed")
         next_page_xpath = response.xpath('//*[@id="content"]/div[3]/a')[-1]
         next_page_text = next_page_xpath.css('::text').extract_first()
